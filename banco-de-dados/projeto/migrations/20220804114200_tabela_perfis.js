84cae3c27d767bc287c64d99a6dfec5bd0849702
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-04 09:22:36 -0300

    Migrations #01

--- banco-de-dados/projeto/migrations/20220804114200_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20220804114200_tabela_perfis.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('perfis', table => {
+    table.increments('id').primary()
+    table.string('nome').notNull().unique()
+    table.string('rotulo').notNull()
+  })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('perfis')
 };
