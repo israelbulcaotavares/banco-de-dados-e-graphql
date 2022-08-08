5c1902f38e4adb4b715265e1a86057c7e3e90a63
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-08 19:06:50 -0300

    Desafio Banco de Dados & GraphQL - Resposta #03

--- banco-de-dados/desafio/resolvers/Mutation/usuario.js
+++ banco-de-dados/desafio/resolvers/Mutation/usuario.js
@@ -2,12 +2,46 @@ const db = require('../../config/db')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+        try{
+            const idsPerfis = []
+            if(dados.perfis){
+                for(filtro of dados.perfis){
+                    const perfil = await obterPerfil(_, {
+                        filtro:{ ...filtro }
+                    })
+                    if(perfil) idsPerfis.push(perfil.id)
+                }
+            }    
+
+            delete dados.perfis  
+            
+            const [id] = await db('usuarios')
+                .insert(dados)
+            
+                for(perfil_id of idsPerfis){
+                    await db('usuarios_perfis')
+                        .insert( { perfil_id, usuario_id: id })
+                }
+
+                return db('usuarios')
+                    .where( { id }).first()
+
+        } catch( e ) {
+            throw new Error(e.sqlMessage)
+        }
     },
     async excluirUsuario(_, { filtro }) {
-        // Implementar
+        try{
+                               
+        } catch( e ) {
+            throw new Error(e.sqlMessage)
+        }
     },
     async alterarUsuario(_, { filtro, dados }) {
-        // Implementar
+        try{
+                               
+        } catch( e ) {
+            throw new Error(e.sqlMessage)
+        }
     }
 }
